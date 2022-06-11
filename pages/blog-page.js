acf99670d41f7a0d913e10d2a3ca61c2ddb12a82
杉山浩平 <kouhei.sugiyama.02@gmail.com>
2022-06-11 19:21:03 +0900

    aaaa

--- pages/blog-page.js
+++ pages/blog-page.js
@@ -1,13 +1,16 @@
 import Layout from "../components/Layout";
-import Post from "../components/post";
-import { getAllPostsData } from "../lib/posts";
+// import Post from "../components/post";
+// import { getAllPostsData } from "../lib/posts";
 
 export default function Blog({ posts }) {
   return (
+    // <Layout title="Blog">
+    //   <ul className="m-10">
+    //     {posts && posts.map((post) => <Post key={post.id} post={post} />)}
+    //   </ul>
+    // </Layout>
     <Layout title="Blog">
-      <ul className="m-10">
-        {posts && posts.map((post) => <Post key={post.id} post={post} />)}
-      </ul>
+      <ul className="m-10">hello</ul>
     </Layout>
   );
 }
